46f04a07534e4f51cf55548925f6e6028e430f18
sunil_swizy <sunil@iamneo.ai>
2023-10-06 16:11:48 +0530

    Description changes

--- src/app.js
+++ src/app.js
@@ -16,9 +16,6 @@ function App() {
       for(let j = 0; j < 100; j++) {
         console.log(i)
     }
-   }
-  }
-  
   const logMe2 = () => {
     for(let i = 0; i < 100; i++) {
       for(let j = 0; j < 100; j++) {
